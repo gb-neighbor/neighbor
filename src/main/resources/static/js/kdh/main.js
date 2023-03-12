4a4dfa1121094387d82b8cafd82c00f4a105e454
dev-AustinKim <ehdgks0412@gmail.com>
2023-03-13 00:44:57 +0900

    김동한 푸터 완료 헤더, 메인페이지 작업중

---
+++ src/main/resources/static/js/kdh/main.js
@@ -0,0 +1,97 @@
+/* banner.html */
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+const banner = document.querySelector("div.banner");
+const imageDiv = document.querySelectorAll("div.banner div");
+const lastImageDiv = document.createElement("div");
+const firstImageDiv = document.createElement("div");
+const next = document.querySelector("div.next");
+const prev = document.querySelector("div.prev");
+const buttons = document.querySelectorAll(".buttons button");
+let checkArrow = false;
+let count = 1;
+let auto = setInterval(autoSlide, 2000);
+let temp = buttons[0];
+const pageNow = document.querySelector("#page-now");
+
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        clearInterval(auto);
+        count = parseInt(button.innerHTML);
+        changeButtonStyle();
+        banner.style.transition = "transform 0.3s";
+        banner.style.transform = `translate(${-1200 * count}px)`;
+        auto = setInterval(autoSlide, 2000);
+    });
+});
+
+imageDiv.forEach((div, i) => div.style.backgroundImage = `url(image/00${i+1}.jpg)`)
+
+banner.appendChild(lastImageDiv);
+lastImageDiv.style.backgroundImage = `url(image/001.jpg)`;
+
+banner.insertBefore(firstImageDiv, document.querySelector("div.banner div"));
+firstImageDiv.style.backgroundImage = `url(image/00${imageDiv.length}.jpg)`
+
+banner.style.transform = `translate(-1200px)`;
+
+function changeButtonStyle() {
+    if(temp){
+        temp.style.background = "white";
+        temp.style.color = "black";
+    }
+    buttons[count - 1].style.background = "black";
+    buttons[count - 1].style.color = "white";
+    temp = buttons[count - 1];
+    pageNow.innerHTML = count;
+}
+
+function autoSlide(){
+    banner.style.transition = "transform 0.3s";
+    banner.style.transform = `translate(${-1200 * ++count}px)`;
+    console.log(count);
+    if(count == 6) {
+        count = 1;
+        setTimeout(function(){
+            banner.style.transition = "transform 0s";
+            banner.style.transform = "translate(-1200px)";
+        }, 300);
+    }
+    changeButtonStyle();
+}
+
+prev.addEventListener("click", function(){
+    if(checkArrow){return;}
+    checkArrow = true;
+    clearInterval(auto);
+    banner.style.transition = "transform 0.3s";
+    banner.style.transform = `translate(${-1200 * --count}px)`;
+    if(count == 0) {
+        count = 5;
+        setTimeout(function(){
+            banner.style.transition = "transform 0s";
+            banner.style.transform = `translate(${-1200 * (imageDiv.length)}px)`;
+        }, 300);
+    }
+    changeButtonStyle();
+    auto = setInterval(autoSlide, 2000);
+    setTimeout(()=>{checkArrow = false}, 300);
+});
+
+next.addEventListener("click", function(){
+    if(checkArrow){return;}
+    checkArrow = true;
+    clearInterval(auto);
+    banner.style.transition = "transform 0.3s";
+    banner.style.transform = `translate(${-1200 * ++count}px)`;
+    if(count == 6) {
+        count = 1;
+        setTimeout(function(){
+            banner.style.transition = "transform 0s";
+            banner.style.transform = "translate(-1200px)";
+        }, 300);
+    }
+    changeButtonStyle();
+    auto = setInterval(autoSlide, 2000);
+    setTimeout(()=>{checkArrow = false}, 300);
+});
