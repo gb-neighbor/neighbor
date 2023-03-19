2545532a597ea8253b2d64d7399cc9cd96796b7a
dev-typelym <dlstk3031@gmail.com>
2023-03-20 02:31:09 +0900

    마이페이지 커밋

---
+++ src/main/resources/static/js/lyt/upload_food.js
@@ -0,0 +1,21 @@
+/* 위로가기 버튼 */
+var topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+topBtn.addEventListener("click", topFunction);
