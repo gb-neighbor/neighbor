19b1f7d86b29c5ffe27d8cd7aa8c23213261a639
dev-francine <test.code0624@gmail.com>
2023-03-24 19:08:02 +0900

    황현수 타임리프 중간 커밋

---
+++ src/main/java/com/neighbor/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.neighbor.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping("test")
+    public String test(){
+        return "list/list-by-member";
+    }
+}
--- src/main/resources/static/js/kjp/product-detail2.js
+++ src/main/resources/static/js/kjp/product-detail2.js
@@ -194,3 +194,25 @@ function pushNext() {
         }, 30);
     }
 };
+
+/* ***************************tothetop 버튼******************************* */
+var topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		topBtn.style.display = "block";
+	} else {
+		topBtn.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+topBtn.addEventListener("click", topFunction);
