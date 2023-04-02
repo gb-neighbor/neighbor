703945aed1a2a5e850853f230c194c36f779597b
dev-typelym <dlstk3031@gmail.com>
2023-04-02 18:26:19 +0900

    세션 멤버 아이디 가져오는거 수정, js 수정

--- src/main/java/com/neighbor/controller/MainController.java
+++ src/main/java/com/neighbor/controller/MainController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main/*")
 @RequiredArgsConstructor
 @Slf4j
 public class MainController {
