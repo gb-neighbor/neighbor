4d71a09246f4c6f66e1d7c6f8f37d9dead6f583a
dev-typelym <dlstk3031@gmail.com>
2023-04-03 19:00:33 +0900

    마이페이지 홈 js 정규식 추가

--- src/main/resources/static/js/mypage/profile_home.js
+++ src/main/resources/static/js/mypage/profile_home.js
@@ -192,3 +192,222 @@ fileDOM.addEventListener('change', () => {
     };
     reader.readAsDataURL(fileDOM.files[0]);
 });
+
+/*정규식*/
+
+
+/* 모든 정보가 있어야만 클릭 가능*/
+function changeButton(){
+    if(checkAll[0] && checkAll[1] && checkAll[2] && checkAll[3] && checkAll[4] && checkAll[5] && checkAll[6]){
+        $(".change-button").attr("disabled", false);
+        $(".password-check").attr("disabled", false);
+    } else {
+        $(".change-button").attr("disabled", true);
+        $(".password-check").attr("disabled", true);
+    }
+}
+
+/* 인풋테그에 값이 있으면 순서대로 true로 변경 */
+let checkAll = [false, false, false, false, false, false];
+/* 순서대로
+    0 -> 닉네임
+    1 -> 닉네임 중복
+    2 -> 생일
+    3 -> 핸드폰 번호
+    4 -> 핸드폰 번호 중복
+    5 -> 이메일
+    6 -> 이메일 중복
+*/
+
+/* 닉네임 */
+$errNick = $(".err-nickname")
+$(".nick-name").on("keyup",function(){
+    if ($(this).val().length > 8){
+        alert("최대 8자까지 입력 가능합니다.");
+        $(this).val($(this).val().substring(0, 9));
+        $('.nick-name').html(9);
+
+    } else if($(this).val()){
+        $errNick.text("")
+        checkAll[0] = true;
+        changeButton();
+
+    }
+
+    if(!$(this).val()){
+        checkAll[0] = false;
+        $errNick.text("닉네임을 입력해주세요")
+        $errNick.css('color', 'red')
+    } else if($(this).val()){
+        $errNick.text("")
+        checkAll[0] = true;
+        changeButton();
+
+    }
+})
+
+/*닉네임 중복*/
+const $joinInputNickname = $(".nick-name");
+const $errorNickname = $(".err-nickname");
+
+$joinInputNickname.on("blur",function(){$.ajax({
+    url:"/members/checkNickname" ,
+    data: {"memberNickname": $joinInputNickname.val()},
+    success: function(result){
+        let message;
+
+        if(result == 1){
+            message = "중복된 닉네임입니다.";
+            $errorNickname.css('color', 'red')
+            $errorNickname.css('display', 'block');
+            checkAll[1] = false;
+
+        }else if($joinInputNickname.val().length < 1){
+            $errorNickname.css('display', 'block');
+            $errorNickname.css('color', 'red');
+            message = "필수 입력 사항입니다";
+            checkAll[1] = false;
+        }else {
+            message = "사용 가능한 닉네임입니다.";
+            $errorNickname.css('display', 'block');
+            $errorNickname.css('color', '#2bb673');
+            checkAll[1] = true;
+            changeButton();
+        }
+        $errorNickname.text(message);
+    }
+});
+});
+
+let regPhone= /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+$(".phone").on("keyup", function(){
+    if($(".phone").val() != "" && $(".phone").val().length == 11 && regPhone.test($(".phone").val())) {
+        checkAll[2] = true;
+        $(".err-phone").text("");
+        changeButton();
+    } else {
+        checkAll[2] = false;
+        $(".err-phone").text("핸드폰 번호를 제대로 입력해주세요")
+        $(".err-phone").css('color', 'red')
+    }
+})
+
+//휴대폰 중복
+const $joinInputPhoneNum = $(".phone");
+const $errorPhoneNum = $(".err-phone");
+
+$joinInputPhoneNum.on("blur",function(){$.ajax({
+    url:"/members/checkPhone" ,
+    data: {"memberPhone": $(".phone").val()},
+    success: function(result){
+        let message;
+
+        if(result == 1){
+            message = "중복된 번호입니다.";
+            $errorPhoneNum.css('color', 'red')
+            $errorPhoneNum.css('display', 'block');
+            checkAll[3] = false;
+
+        }else if($joinInputPhoneNum.val().length < 1){
+            $errorPhoneNum.css('display', 'block');
+            $errorPhoneNum.css('color', 'red');
+            message = "필수 입력 사항입니다";
+            checkAll[3] = false;
+
+        }else if (!regPhone.test($(".phone").val())) {
+            $(".err-phone").text("번호 형식이 유효하지 않습니다.")
+            $(".err-phone").css('color', 'red')
+            checkAll[3] = false;
+        }else {
+            message = "사용 가능한 번호입니다.";
+            $errorPhoneNum.css('display', 'block');
+            $errorPhoneNum.css('color', '#2bb673');
+            checkAll[3] = true;
+            changeButton();
+        }
+        $errorPhoneNum.text(message);
+    }
+});
+});
+
+
+//이메일
+const $email = $(".email");
+const $helpEmail = $(".err-email")
+
+var regExp = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+$email.keyup(function(){
+    if( !$email.val() ){
+        $helpEmail.text("이메일을 입력해주세요")
+        $errEmail.css('color', 'red')
+        $email.focus();
+        checkAll[4] = false;
+    } else if (!regExp.test($email.val())) {
+        $helpEmail.text("이메일이 유효하지 않습니다.")
+        $errEmail.css('color', 'red')
+        $email.focus();
+        checkAll[4] = false;
+    } else {
+        $helpEmail.text("");
+        checkAll[4] = true;
+        changeButton();
+
+    }
+
+})
+
+//이메일 중복
+const $inputEmail = $(".email");
+const $errEmail = $(".err-email");
+
+$inputEmail.on("blur", function(){$.ajax({
+    url:"/members/checkEmail" ,
+    data: {"memberEmail": $inputEmail.val()},
+    success: function(result){
+        let message;
+        if(result == 1){
+            message = "중복된 이메일입니다.";
+            $errEmail.css('color', 'red')
+            $errEmail.css('display', 'block');
+            checkAll[5] = false;
+
+        } else if($inputEmail.val().length < 1){
+            $errEmail.css('display', 'block');
+            $errEmail.css('color', 'red');
+            message = "필수 입력 사항입니다";
+            checkAll[5] = false;
+
+        } else if(!regExp.test($inputEmail.val())){
+            $errEmail.css('display', 'block');
+            $errEmail.css('color', 'red');
+            message = "이메일 형식에 맞춰주세요";
+            checkAll[5] = false;
+
+        } else{
+            message = "사용 가능한 이메일입니다.";
+            $errEmail.css('display', 'block');
+            $errEmail.css('color', '#2bb673');
+            checkAll[5] = true;
+            changeButton();
+        }
+
+        $errEmail.text(message);
+    }
+});
+});
+
+/* 생일 */
+let regBirth = /^(19[0-9][0-9]|20\d{2})(0[0-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$/;
+
+$(".birth").on("keyup", function() {
+    if($(this).val() != "" && $(this).val().length == 8 && regBirth.test($(this).val())){
+        checkAll[6] = true;
+        $(".err-birth").text("");
+        changeButton();
+    } else {
+        checkAll[6] = false;
+        $(".err-birth").text("생일을 정확히 입력해주세요")
+        $(".err-birth").css('color', 'red')
+    }
+});
