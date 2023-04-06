355cc0546960adaf6a60a14f3aac2c05b296048b
dev-Adward <youchanj66@gmail.com>
2023-04-06 16:55:35 +0900

    수정

--- src/main/resources/static/js/list/list-by-region.js
+++ src/main/resources/static/js/list/list-by-region.js
@@ -313,6 +313,7 @@ function appendList(boardDTOList) {
 // 페이지 로딩 시 초기 리스트를 불러옴
 boardService.getList(function(boardDTOList) {
     appendList(boardDTOList);
+    getProfileImage();
 });
 
 // 검색창에서 키보드를 눌렀을 때
@@ -340,6 +341,7 @@ $("button[name=search]").on("click", function(){
                     console.log(page)
                     boardService.getList(function(boardDTOList) {
                         appendList(boardDTOList);
+                        getProfileImage()
                     });
                 }
             }
@@ -366,6 +368,7 @@ $("select[name=gugun]").on("change", function () {
                     console.log(page)
                     boardService.getList(function(boardDTOList) {
                         appendList(boardDTOList);
+                        getProfileImage()
                     });
                 }
             }
@@ -373,7 +376,7 @@ $("select[name=gugun]").on("change", function () {
     });
 });
 
-
+getProfileImage()
 //지도
 $(".input-gugun").on("click", function () {
     gugun = $(this).prev().val();
@@ -417,6 +420,7 @@ $(window).on('scroll', function() {
             console.log(page)
             boardService.getList(function(boardDTOList) {
                 appendList(boardDTOList);
+                getProfileImage()
             });
         }
     }
@@ -451,7 +455,6 @@ function generateStarHtml(avgScore) {
     }
     return stars;
 }
-
 function getProfileImage(){
     $('.thumbs1').each(function () {
         let boardFilePath = $(this).data('board-file-path');
